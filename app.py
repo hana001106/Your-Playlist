9b09a1ac2d5415ce22fd34fa24cafcf40ee8b1af
Kim Hana <110812273+hana001106@users.noreply.github.com>
2023-11-30 01:26:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,8 @@ def translate_text(text, target_language='en'):
     return translation
 
 # API 키 설정 (실제 키로 대체해야 함)
-# api_key = "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"
-# openai.api_key = api_key
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+api_key = "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"
+openai.api_key = api_key
 
 st.set_page_config(
     page_title="너만의 플레이리스트 너플리",
